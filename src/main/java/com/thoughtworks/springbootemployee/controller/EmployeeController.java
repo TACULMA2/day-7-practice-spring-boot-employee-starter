4b05706b13e23475605938e2ad75f59c3191cc8e
TACULMA2 <marigold.taculog@oocl.com>
2023-08-22 19:46:00 +0800

    feat: add EmployeeNotFoundException Class, add findByID method, add list of employees

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> listAll() {
         return employeeRepository.listAll();
+    }
+
+    @GetMapping (path = "/{id}")
+    public Employee findById (@PathVariable Long id) {
+        return employeeRepository.findById(id);
     }
 }
--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
@@ -10,11 +10,25 @@ public class EmployeeRepository {
     private static final List<Employee> employees = new ArrayList<>();
 
     static {
-        employees.add(new Employee(1L, "Alice", 30, "Female", 5000));
-
+        employees.add(new Employee(1L, "Monkey D. Luffy", 19, "Male", 300000));
+        employees.add(new Employee(2L, "Ronoroa Zoro", 21, "Male", 111100));
+        employees.add(new Employee(3L, "Nami", 20, "Female", 36600));
+        employees.add(new Employee(4L, "Ussop", 19, "Male", 50000));
+        employees.add(new Employee(5L, "Vinsmoke Sanji", 21, "Male", 103200));
+        employees.add(new Employee(6L, "Tony Tony Chopper", 17, "Male", 1000));
+        employees.add(new Employee(7L, "Nico Robin", 30, "Female", 93000));
+        employees.add(new Employee(8L, "Franky", 36, "Male", 39400));
+        employees.add(new Employee(9L, "Brook", 90, "Male", 38300));
     }
 
     public List<Employee> listAll() {
         return employees;
+    }
+
+    public Employee findById(Long id) {
+        return employees.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElseThrow(EmployeeNotFoundException::new);
     }
 }
