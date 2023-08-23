7ae870fa65b3a3be28a8f71c6e8215ebf66abdc9
TACULMA2 <marigold.taculog@oocl.com>
2023-08-23 20:00:04 +0800

    test: add test case should_return_list_of_companies_with_given_range_when_get_companies_given_pageNumber_and_pageSize()

--- src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -105,4 +106,39 @@ public class CompanyApiTests {
                 .andExpect(jsonPath("$.companyName").value(updatedCompany.getCompanyName()));
     }
 
+    @Test
+    void should_return_no_content_from_same_company_id_when_perform_delete_given_a_company_id() throws Exception{
+        //given
+        Company deleteCompany = companyRepository.addCompany(new Company(1L,"OOCL"));
+        //when
+        mOckMvcClient.perform(MockMvcRequestBuilders.delete("/companies/"+ deleteCompany.getCompanyId()))
+                .andExpect(status().isNoContent());
+        //then
+        Assertions.assertTrue(companyRepository.listAllCompanies().isEmpty());
+    }
+
+    @Test
+    void should_return_list_of_companies_with_given_range_when_get_companies_given_pageNumber_and_pageSize() throws Exception {
+        //given
+        Company oocl = companyRepository.addCompany(new Company(1l, "OOCL"));
+        Company cosco = companyRepository.addCompany(new Company(2l, "COSCO"));
+        Company tw = companyRepository.addCompany(new Company(3l, "TW"));
+        Company scape = companyRepository.addCompany(new Company(4l, "SCAPE"));
+        Company dd = companyRepository.addCompany(new Company(5l, "DD"));
+        Long pageNumber = 1L;
+        Long pageSize = 4L;
+        //when
+        String queryString = String.format("/companies?pageNumber=%d&pageSize=%d", pageNumber, pageSize);
+        mOckMvcClient.perform(MockMvcRequestBuilders.get(queryString))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].companyId").value(oocl.getCompanyId()))
+                .andExpect(jsonPath("$[0].companyName").value(oocl.getCompanyName()))
+                .andExpect(jsonPath("$[1].companyId").value(cosco.getCompanyId()))
+                .andExpect(jsonPath("$[1].companyName").value(cosco.getCompanyName()))
+                .andExpect(jsonPath("$[2].companyId").value(tw.getCompanyId()))
+                .andExpect(jsonPath("$[2].companyName").value(tw.getCompanyName()))
+                .andExpect(jsonPath("$[3].companyId").value(scape.getCompanyId()))
+                .andExpect(jsonPath("$[3].companyName").value(scape.getCompanyName()));
+    }
 }
