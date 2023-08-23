46dab638c634f874179b67c7d0eaca4fc1c0f331
TACULMA2 <marigold.taculog@oocl.com>
2023-08-23 19:35:13 +0800

    test: add test case should_return_the_companies_when_perform_post_companies_given_a_new_company_with_JSON_format()

--- src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyApiTests.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -73,5 +76,18 @@ public class CompanyApiTests {
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].companyId").value(oocl.getCompanyId()))
                 .andExpect(jsonPath("$[0].companyName").value(oocl.getCompanyName()));
+    }
+
+    @Test
+    void should_return_the_companies_when_perform_post_companies_given_a_new_company_with_JSON_format() throws Exception {
+        //given
+        Company newCompany = new Company("COSCO");
+        //when
+        mOckMvcClient.perform(MockMvcRequestBuilders.post("/companies/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(newCompany)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.companyId").value(notNullValue()))
+                .andExpect(jsonPath("$.companyName").value(newCompany.getCompanyName()));
     }
 }
